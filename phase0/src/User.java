867dd368bfeb21b8eadabfb07809e1ed47d82bad
Chaitanya Kumar Mahajan <chait.mahajan@mail.utoronto.ca>
2022-06-07 19:37:59 -0600

    current progress of user Class, and created empty Event Class

--- phase0/src/User.java
+++ phase0/src/User.java
@@ -1,7 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class User {
 
+    private String password;
+    private String username;
+    private boolean isAdmin;
+    private List<Event> events;
+
+    public User(String username, String password, boolean isAdmin) {
+
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.username = username;
+        this.events = new ArrayList<>();
+    }
+
+    /**
+     * Returns ture if new password is not the same as current password and changes password to newPassword
+     * @param newPassword a new password to replace current password
+     * @return true if password change was successful, false otherwise.
+     */
+    public boolean setPassword(String newPassword) {
+        if (!this.password.equals(newPassword)) {
+            this.password = newPassword;
+
+            Event event = Event();
+            this.events.add(event);
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return weather this User is an admin
+     */
+    public boolean getIsAdmin() {
+        return this.isAdmin;
+    }
 
 
-    // Major Methods
 
 }
