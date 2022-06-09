28eb6fcd192dc218db86f421919eeddc4f70911d
vyknight <knightadark101@gmail.com>
2022-06-09 15:21:08 -0600

    Wrote UserManager, created addBasicUser, addAdminUser, and getUsernames methods. Also added a constructor to Basic User.

--- phase0/src/BasicUser.java
+++ phase0/src/BasicUser.java
@@ -1,4 +1,6 @@
 
 public class BasicUser extends User{
-
+    public BasicUser(String username, String password){
+        super(username, password, false);
+    }
 }
--- phase0/src/UserManager.java
+++ phase0/src/UserManager.java
@@ -1,14 +1,58 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+
 
 public class UserManager {
-    public static boolean addUser(User newUser){
-        // TODO: implement this function
-        return false;
+    private static final HashMap<User, String> users = new HashMap<>();
+
+    // using default constructor for now because everything is static
+
+    /**
+     * create a basic user and add it to the users collection
+     * return true if successful, false if username is already taken
+     * @param username username of this user
+     * @param password password of this user
+     * @return true if user creation was successful, false otherwise
+     */
+    public static boolean addBasicUser(String username, String password){
+        if(users.containsValue(username)){
+            // username already exists !
+            return false;
+        }
+        else{
+            BasicUser u = new BasicUser(username, password);
+            users.put(u, username);
+            return true;
+        }
     }
 
+    /**
+     * create an admin user and add it to the users collection
+     * return true if successful, false if username is already taken
+     * @param username username of this user
+     * @param password password of this user
+     * @return true if user creation was successful, false otherwise
+     */
+    public static boolean addAdminUser(String username, String password){
+        if(users.containsValue(username)){
+            // username already exists !
+            return false;
+        }
+        else{
+            AdminUser u = new AdminUser(username, password);
+            users.put(u, username);
+            return true;
+        }
+    }
+
+
+    /**
+     * return a string list of all the usernames stored in the user manager
+     * @return ArrayList<String> of usernames
+     */
     public static List<String> getUsernames(){
-        return new ArrayList<>();
+        return new ArrayList<>(users.values());
     }
 
 }
