a11615aea2c8341830ab08e905a15c5aa9e86b2e
kiransureshan <82007113+kiransureshan@users.noreply.github.com>
2022-06-09 15:30:23 -0400

    added AdminUser and amended abstract type of 2 methods in User class

---
+++ phase0/src/AdminUser.java
@@ -0,0 +1,40 @@
+public class AdminUser extends User{
+    /**
+     * Creates a new AdminUser with username, and password, and type Admin.
+     * @param username username of this User
+     * @param password password used by this User to login
+     */
+    public AdminUser(String username, String password){
+        super(username,password, true);
+    }
+    /**
+     * Creates either a Basic/Admin user with username, and password.
+     * @param username username of this User
+     * @param password password used by this User to login
+     * @param isAdmin boolean representing the type of User
+     */
+    public boolean createUser(String username, String password, boolean isAdmin){
+        boolean creationSuccess;
+        if (isAdmin){
+            creationSuccess = UserManager.addUser(new AdminUser(username, password));
+        } else {
+            creationSuccess = UserManager.addUser(new BasicUser(username, password));
+        }
+        return creationSuccess;
+    }
+    /**
+     * Bans the given user.
+     * @param user user to ban
+     */
+    public boolean banUser(BasicUser user){
+        user.isBanned = true;
+        return true;
+    }
+    /**
+     * Deletes the given user.
+     * @param user user to delete
+     */
+    public boolean deleteUser(BasicUser user){
+        return UserManager.deleteUser(user);
+    }
+}
--- phase0/src/UserManager.java
+++ phase0/src/UserManager.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserManager {
+    public static boolean addUser(User newUser){
+        // TODO: implement this function
+        return false;
+    }
+
+    public static List<String> getUsernames(){
+        return new ArrayList<>();
+    }
+
 }
