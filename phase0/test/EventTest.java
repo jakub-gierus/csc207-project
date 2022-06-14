141daaf71ea666011d5511c5c2ef6a2f6a222f02
Andrew Hu <an.hu@mail.utoronto.ca>
2022-06-14 16:33:11 -0700

    ChangePasswordEvent and LoginEvent

--- phase0/test/EventTest.java
+++ phase0/test/EventTest.java
@@ -1,3 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import src.UserManager;
+
 class EventTest {
     @BeforeEach
     void setup() {
