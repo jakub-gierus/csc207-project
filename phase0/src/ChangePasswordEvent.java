8361e51ce60bf6a4fb5649f999289f9ebb0050c7
Andrew Hu <an.hu@mail.utoronto.ca>
2022-06-14 13:04:48 -0700

    ChangePasswordEvent and LoginEvent

--- phase0/src/ChangePasswordEvent.java
+++ phase0/src/ChangePasswordEvent.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 
-public class ChangePasswordEvent extends Event{
+public class ChangePasswordEvent extends Event {
 
     //private String prevPwd -> previous passowrd
     //private String newPwd -> new passowrd
+
     /**
      * Creates a new Event with the current date and time, and a message.
+     *
      * @param message message of event
      */
-    public ChangePasswordEvent(List<User> assciatedUsers, String message){
+    public ChangePasswordEvent(List<User> assciatedUsers, String message) {
         super(assciatedUsers, message);
     }
 }
--- phase0/src/LoginEvent.java
+++ phase0/src/LoginEvent.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 
-public class LoginEvent extends Event{
+public class LoginEvent extends Event {
     /**
      * Creates a new Event with the current date and time, and a message.
+     *
      * @param message message of event
      */
     public LoginEvent(List<User> assciatedUsers, String message) {
