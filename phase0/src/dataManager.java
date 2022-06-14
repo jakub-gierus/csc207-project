26865d46cb60cc4ca91010e6d64478015f877973
Matthew <mz.liu@mail.utoronto.ca>
2022-06-14 16:26:17 -0600

    Created stub dataManager class

---
+++ phase0/src/dataManager.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class dataManager {
+
+    public dataManager (String location, String filename){
+
+    }
+
+    /**
+     * returns a hashmap in the following format: (String username, User user_objects)
+     */
+    public HashMap loadUsers(){
+
+    }
+
+    /**
+     * rewrite the file
+     * @param h hashmap of users
+     */
+    public void rewriteFile(HashMap h){
+
+    }
+
+}
