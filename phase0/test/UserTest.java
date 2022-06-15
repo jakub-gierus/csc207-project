adc93b68d071df96d485fae42c10d7279a370a9d
Chaitanya Kumar Mahajan <chait.mahajan@mail.utoronto.ca>
2022-06-15 11:20:41 -0600

    more tests

--- phase0/test/UserTest.java
+++ phase0/test/UserTest.java
@@ -2,20 +2,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 public class UserTest {
 
     @Before
-    public void setUp() throws Exception {
-    UserManager.addBasicUser("Basic1", "Basic1");
-    UserManager.addAdminUser("Admin1", "Admin1");
+    public void setUp() {
+        try {
+            UserManager.addBasicUser("Basic1", "Basic1");
+            UserManager.addAdminUser("Admin1", "Admin1");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
     }
 
     @Test
@@ -23,6 +29,12 @@ public class UserTest {
         try {
             UserUseClass uuc = new UserUseClass();
             assertFalse(uuc.setUsername("Admin1", "Basic1"));
+            assertTrue(uuc.setUsername("admin1", "Admin1"));
+            List<String> lst = new ArrayList<>();
+            lst.add("admin1");
+            lst.add("Basic1");
+            assertEquals("UserManager's usernames is not correctly updated after username change",
+                    lst, UserManager.getUsernames());
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -88,6 +100,55 @@ public class UserTest {
             assertTrue(uuc.login("Basic1", "Basic1"));
             assertTrue(u2.getIsLoggedIn());
 
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testTempBan() {
+        AdminUser u1 = (AdminUser) UserManager.getUser("Admin1");
+        BasicUser u2 = (BasicUser) UserManager.getUser("Basic1");
+        try {
+            UserUseClass uuc = new UserUseClass();
+            assertTrue(uuc.login("Admin1", "Admin1"));
+            assertTrue(uuc.login("Basic1", "Basic1"));
+
+            assertTrue(u1.tempBanUser("Basic1"));
+            assertFalse(u2.getIsLoggedIn());
+            assertTrue(u2.getIsTempBan());
+
+            assertFalse(uuc.login("Basic1", "Basic1"));
+
+            // Change BasicUser Time to 1 Min
+            TimeUnit.MINUTES.sleep(1);
+
+            assertTrue(uuc.login("Basic1", "Basic1"));
+            assertTrue(u2.getIsLoggedIn());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testLogin_After_Username_Change() {
+        AdminUser u1 = (AdminUser) UserManager.getUser("Admin1");
+        BasicUser u2 = (BasicUser) UserManager.getUser("Basic1");
+        try {
+            UserUseClass uuc = new UserUseClass();
+            assertTrue(uuc.login("Admin1", "Admin1"));
+            assertTrue(uuc.login("Basic1", "Basic1"));
+            assertTrue(u1.getIsLoggedIn());
+            assertTrue(u2.getIsLoggedIn());
+
+            assertTrue(uuc.logOut("Basic1"));
+            assertTrue(uuc.setUsername("basic1", "Basic1"));
+            assertTrue(u2.setPassword("basic1"));
+
+            assertTrue("UserManager usernames not updated correctly",
+                    uuc.login( "basic1", "basic1"));
 
         } catch (Exception e) {
             e.printStackTrace();
