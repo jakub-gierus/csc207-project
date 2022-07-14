bef22480b4bcb80703d28257d4506b911199a2c5
kiransureshan <82007113+kiransureshan@users.noreply.github.com>
2022-07-14 14:47:31 -0400

    added art checking for art library and empty class files

--- phase1/src/manager/ArtLibrary.java
+++ phase1/src/manager/ArtLibrary.java
@@ -3,13 +3,31 @@ package manager;
 import entity.Art;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
+/**
+ * Singleton class
+ * Keeps track of every piece of art
+ **/
 public class ArtLibrary {
-    /*
-    This should be a singleton
-    Keeps track of every piece of art
-     */
-    private HashMap<String, Art> map = new HashMap<>();
 
+    // Set containing all the art contained in the system/app
+    // name : Art
+    private HashMap<String, Art> library = new HashMap<>();
+
+    /**
+     * Checks if a given instance of art piece exists within the system
+     * @return boolean representing if the art instance exists
+     * @param art piece to check
+     */
+    public boolean artExists(Art art){
+        return this.library.containsKey(art.getTitle());
+    }
+
+    public boolean artExists(){
+        // temp
+        return false;
+    }
+    
 
 }
