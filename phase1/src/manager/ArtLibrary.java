1cc7da68e53a3b8c832f5a8b9369184fa434d35f
kiransureshan <82007113+kiransureshan@users.noreply.github.com>
2022-07-14 16:21:25 -0400

    added artlibrary methods and removed uniquenesschecker

--- phase1/src/manager/ArtLibrary.java
+++ phase1/src/manager/ArtLibrary.java
@@ -4,30 +4,48 @@ import entity.Art;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Optional;
 
 /**
- * Singleton class
+ * SINGLETON class
  * Keeps track of every piece of art
  **/
 public class ArtLibrary {
-
     // Set containing all the art contained in the system/app
-    // name : Art
-    private HashMap<String, Art> library = new HashMap<>();
+    // unique ascii string : Art object
+    final private HashMap<String, Art> library = new HashMap<>();
 
     /**
-     * Checks if a given instance of art piece exists within the system
-     * @return boolean representing if the art instance exists
+     * Returns true if an art piece exists in the library, false otherwise
      * @param art piece to check
      */
     public boolean artExists(Art art){
-        return this.library.containsKey(art.getTitle());
+        return this.library.containsKey(art.getArt());
     }
 
-    public boolean artExists(){
-        // temp
-        return false;
+    /**
+     * Returns an art piece if it exists in the library, null otherwise
+     * @param ascii -> title of art piece*/
+    public Art getArt(String ascii){
+        return this.library.containsKey(ascii) ? library.get(ascii) : null;
     }
-    
+    /**
+     * Returns true of the art was successfully added to the library, false otherwise
+     * Adds an art piece to the library if it does not already exist in the library
+     * @param art -> the new art piece to add to the library
+     * */
+    public boolean addArt(Art art){
+        // unique check
+        if(artExists(art)){
+            return false;
+        }
+        //add to library
+        this.library.put(art.getArt(), art);
+        return true;
+    }
+
+
+
+
 
 }
