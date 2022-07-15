d015b20f869cc17e340eb0c954f6d6ba52dd6673
kiransureshan <82007113+kiransureshan@users.noreply.github.com>
2022-07-15 14:54:32 -0400

    modified ArtLibrary to store UUID as key in library storage

--- phase1/src/manager/ArtLibrary.java
+++ phase1/src/manager/ArtLibrary.java
@@ -5,6 +5,7 @@ import entity.Art;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
+import java.util.UUID;
 
 /**
  * SINGLETON class
@@ -12,22 +13,22 @@ import java.util.Optional;
  **/
 public class ArtLibrary {
     // Set containing all the art contained in the system/app
-    // unique ascii string : Art object
-    final private HashMap<String, Art> library = new HashMap<>();
+    // unique id : Art object
+    final private HashMap<UUID, Art> library = new HashMap<>();
 
     /**
      * Returns true if an art piece exists in the library, false otherwise
      * @param art piece to check
      */
     public boolean artExists(Art art){
-        return this.library.containsKey(art.getArt());
+        return this.library.containsKey(art.getId());
     }
 
     /**
      * Returns an art piece if it exists in the library, null otherwise
-     * @param ascii -> title of art piece*/
-    public Art getArt(String ascii){
-        return this.library.containsKey(ascii) ? library.get(ascii) : null;
+     * @param id -> unique id of art piece*/
+    public Art getArt(UUID id){
+        return this.library.containsKey(id) ? library.get(id) : null;
     }
     /**
      * Returns true of the art was successfully added to the library, false otherwise
@@ -40,7 +41,7 @@ public class ArtLibrary {
             return false;
         }
         //add to library
-        this.library.put(art.getArt(), art);
+        this.library.put(art.getId(), art);
         return true;
     }
 
