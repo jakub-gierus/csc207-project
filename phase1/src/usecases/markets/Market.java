4649e50f53ca626be800691d6d523cf6ef335afd
Amane Takeuchi <amane.takeuchi2002@gmail.com>
2022-07-19 16:33:21 -0700

    deleted unnecessary method in Market class

--- phase1/src/usecases/markets/Market.java
+++ phase1/src/usecases/markets/Market.java
@@ -2,6 +2,7 @@ package usecases.markets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
+import java.util.Map;
 
 import entity.art.Art;
 import entity.markets.Wallet;
@@ -57,7 +58,4 @@ public class Market {
         return artLibrary;
     }
 
-    public List<Merchandise> getItemsForSale() {
-        return getitemforsale();
-    }
 }
